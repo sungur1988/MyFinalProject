9c08351cec436c6496bd6cd2a098f72eff160e38
cihan sungur <sungur88@windowslive.com>
2021-02-06 23:24:13 +0300

    06/02/2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,16 +10,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+
+            Console.WriteLine("---------------");
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
             //productManager.Add(new Product { CategoryId=2,ProductName="Cihan",UnitPrice=1000,UnitsInStock=1 });
-            
-            productManager.Delete(new Product {ProductId=78, CategoryId = 2, ProductName = "Cihan", UnitPrice = 1000, UnitsInStock = 1 });
+
+          
             foreach (var item in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(item.ProductId);
             }
-
-            Console.WriteLine("Hello World!");
         }
     }
 }
